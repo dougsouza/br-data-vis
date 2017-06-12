5eea78b6fb8017997e8374585513b42b1e6d817e
Martin Duarte <martinduartemore@gmail.com>
2017-06-12 16:11:43 -0300

    Clean up treemap implementation

--- index.js
+++ index.js
@@ -63,154 +63,162 @@ function update_details(state, postal, year)
     // update state name
     state_name.innerHTML = state;
     current_state = postal;
-    console.log("current state: " + current_state);
 
-    // reset treemap
-    $("#chart").html("");
-
-    // create new treemap
+    // load treemap data
     var file_name = "br-data/bolsas_" + year + ".json";
     d3.json(file_name, function(data) {
-        console.log(data);
         for(var i=0; i<data.length; i++)
         {
             if(data[i].key == postal)
             {
-                console.log(data[i]);
-                main({title: "Scholarships distribution"}, data[i]);
+                treemap_create(data[i]);
                 break;
             }
         }
     });
 }
 
-var defaults = {
-    margin: {top: 24, right: 0, bottom: 0, left: 0},
-    rootname: "TOP",
-    format: ",d",
-    title: "",
-    width: 960,
-    height: 500
-};
+/* TREEMAP VARIABLES */
+var margin = {top:24, right:15, bottom:0, left:0},
+    title_height = 36 + 16,
+    width = $("#details-container").width(),
+    height = $("#details-container").height() * 0.95,
+    format_number = d3.format(",d"),
+    transitioning;
 
-defaults.width = document.getElementById("details-container").offsetWidth;
-defaults.height = document.getElementById("details-container").offsetHeight/1.5;
+width = width - margin.left - margin.right;
+height = height - margin.top - margin.bottom - title_height;
+ 
+var x = d3.scale.linear()
+    .domain([0, width])
+    .range([0, width]);
 
-function main(o, data) {
-  var root,
-      opts = $.extend(true, {}, defaults, o),
-      formatNumber = d3.format(opts.format),
-      rname = opts.rootname,
-      margin = opts.margin,
-      theight = 36 + 16;
+var y = d3.scale.linear()
+    .domain([0, height])
+    .range([0, height]);
 
-  $('#chart').width(opts.width).height(opts.height);
-  var width = opts.width - margin.left - margin.right,
-      height = opts.height - margin.top - margin.bottom - theight,
-      transitioning;
-  
-  var color = d3.scale.category20c();
-  
-  var x = d3.scale.linear()
-      .domain([0, width])
-      .range([0, width]);
-  
-  var y = d3.scale.linear()
-      .domain([0, height])
-      .range([0, height]);
-  
-  var treemap = d3.layout.treemap()
-      .children(function(d, depth) { return depth ? null : d._children; })
-      .sort(function(a, b) { return a.value - b.value; })
-      .ratio(height / width * 0.5 * (1 + Math.sqrt(5)))
-      .round(false);
-  
-  var svg = d3.select("#chart").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.bottom + margin.top)
-      .style("margin-left", -margin.left + "px")
-      .style("margin.right", -margin.right + "px")
+var root_name = "TOP",
+    treemap_title = "Scholarship Distribution",
+    treemap_color = d3.scale.category20c();
+
+var treemap = d3.layout.treemap()
+    .children(function(d, depth) { return depth ? null : d._children; })
+    .sort(function(a, b) { return a.value - b.value; })
+    .ratio(height / width * 0.5 * (1 + Math.sqrt(5)))
+    .round(false);
+
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.bottom + margin.top)
+    .style("margin-left", -margin.left + "px")
+    .style("margin.right", -margin.right + "px")
     .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-      .style("shape-rendering", "crispEdges");
-  
-  var grandparent = svg.append("g")
-      .attr("class", "grandparent");
-  
-  grandparent.append("rect")
-      .attr("y", -margin.top)
-      .attr("width", width)
-      .attr("height", margin.top);
-  
-  grandparent.append("text")
-      .attr("x", 6)
-      .attr("y", 6 - margin.top)
-      .attr("dy", ".75em");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .style("shape-rendering", "crispEdges");
 
-  if (opts.title) {
-    $("#chart").prepend("<p class='treemap-title'>" + opts.title + "</p>");
-  }
-  if (data instanceof Array) {
-    root = { key: rname, values: data };
-  } else {
-    root = data;
-  }
+var grandparent = svg.append("g")
+    .attr("class", "grandparent");
 
-  initialize(root);
-  accumulate(root);
-  layout(root);
-  //console.log(root);
-  display(root);
+grandparent.append("rect")
+    .attr("y", -margin.top)
+    .attr("width", width)
+    .attr("height", margin.top);
 
-  if (window.parent !== window) {
-    var myheight = document.documentElement.scrollHeight || document.body.scrollHeight;
-    window.parent.postMessage({height: myheight}, '*');
-  }
+grandparent.append("text")
+    .attr("x", 6)
+    .attr("y", 6 - margin.top)
+    .attr("dy", ".75em");
 
-  function initialize(root) {
+
+/* ZOOMABLE TREEMAP IMPLEMENTATION */
+function treemap_create(root)
+{
+    treemap_initialize(root, width, height);
+    treemap_accumulate(root);
+    treemap_layout(treemap, root);
+    treemap_display(root);
+    //console.log(root);
+}
+
+function treemap_initialize(root)
+{
     root.x = root.y = 0;
     root.dx = width;
     root.dy = height;
     root.depth = 0;
-  }
+}
 
-  // Aggregate the values for internal nodes. This is normally done by the
-  // treemap layout, but not here because of our custom implementation.
-  // We also take a snapshot of the original children (_children) to avoid
-  // the children being overwritten when when layout is computed.
-  function accumulate(d) {
+// Aggregate the values for internal nodes. This is normally done by the
+// treemap layout, but not here because of our custom implementation.
+// We also take a snapshot of the original children (_children) to avoid
+// the children being overwritten when when layout is computed.
+function treemap_accumulate(d)
+{
     return (d._children = d.values)
-        ? d.value = d.values.reduce(function(p, v) { return p + accumulate(v); }, 0)
-        : d.value;
-  }
+    ? d.value = d.values.reduce(function(p, v) { return p + treemap_accumulate(v); }, 0)
+    : d.value;
+}
 
-  // Compute the treemap layout recursively such that each group of siblings
-  // uses the same size (1×1) rather than the dimensions of the parent cell.
-  // This optimizes the layout for the current zoom state. Note that a wrapper
-  // object is created for the parent node for each group of siblings so that
-  // the parent’s dimensions are not discarded as we recurse. Since each group
-  // of sibling was laid out in 1×1, we must rescale to fit using absolute
-  // coordinates. This lets us use a viewport to zoom.
-  function layout(d) {
+// Compute the treemap layout recursively such that each group of siblings
+// uses the same size (1×1) rather than the dimensions of the parent cell.
+// This optimizes the layout for the current zoom state. Note that a wrapper
+// object is created for the parent node for each group of siblings so that
+// the parent’s dimensions are not discarded as we recurse. Since each group
+// of sibling was laid out in 1×1, we must rescale to fit using absolute
+// coordinates. This lets us use a viewport to zoom.
+function treemap_layout(treemap, d)
+{
     if (d._children) {
-      treemap.nodes({_children: d._children});
-      d._children.forEach(function(c) {
+        treemap.nodes({_children: d._children});
+        d._children.forEach(function(c) {
         c.x = d.x + c.x * d.dx;
         c.y = d.y + c.y * d.dy;
         c.dx *= d.dx;
         c.dy *= d.dy;
         c.parent = d;
-        layout(c);
-      });
+        treemap_layout(treemap, c);
+        });
     }
-  }
+}
 
-  function display(d) {
+function treemap_text(text)
+{
+    text.selectAll("tspan")
+        .attr("x", function(d) { return x(d.x) + 6; })
+    text.attr("x", function(d) { return x(d.x) + 6; })
+        .attr("y", function(d) { return y(d.y) + 6; })
+        .style("opacity", function(d) { return this.getComputedTextLength() < x(d.x + d.dx) - x(d.x) ? 1 : 0; });
+}
+
+function treemap_text2(text)
+{
+    text.attr("x", function(d) { return x(d.x + d.dx) - this.getComputedTextLength() - 6; })
+        .attr("y", function(d) { return y(d.y + d.dy) - 6; })
+        .style("opacity", function(d) { return this.getComputedTextLength() < x(d.x + d.dx) - x(d.x) ? 1 : 0; });
+}
+
+function treemap_rect(rect)
+{
+    rect.attr("x", function(d) { return x(d.x); })
+        .attr("y", function(d) { return y(d.y); })
+        .attr("width", function(d) { return x(d.x + d.dx) - x(d.x); })
+        .attr("height", function(d) { return y(d.y + d.dy) - y(d.y); });
+}
+
+function treemap_name(d)
+{
+    return d.parent
+        ? treemap_name(d.parent) + " / " + d.key + " (" + format_number(d.value) + ")"
+        : d.key + " (" + format_number(d.value) + ")";
+}
+
+function treemap_display(d)
+{
     grandparent
         .datum(d.parent)
         .on("click", transition)
-      .select("text")
-        .text(name(d));
+        .select("text")
+        .text(treemap_name(d));
 
     var g1 = svg.insert("g", ".grandparent")
         .datum(d)
@@ -218,7 +226,7 @@ function main(o, data) {
 
     var g = g1.selectAll("g")
         .data(d._children)
-      .enter().append("g");
+        .enter().append("g");
 
     g.filter(function(d) { return d._children; })
         .classed("children", true)
@@ -226,21 +234,21 @@ function main(o, data) {
 
     var children = g.selectAll(".child")
         .data(function(d) { return d._children || [d]; })
-      .enter().append("g");
+        .enter().append("g");
 
     children.append("rect")
         .attr("class", "child")
-        .call(rect)
-      .append("title")
-        .text(function(d) { return d.key + " (" + formatNumber(d.value) + ")"; });
+        .call(treemap_rect)
+        .append("title")
+        .text(function(d) { return d.key + " (" + format_number(d.value) + ")"; });
     children.append("text")
         .attr("class", "ctext")
         .text(function(d) { return d.key; })
-        .call(text2);
+        .call(treemap_text2);
 
     g.append("rect")
         .attr("class", "parent")
-        .call(rect);
+        .call(treemap_rect);
 
     var t = g.append("text")
         .attr("class", "ptext")
@@ -250,88 +258,57 @@ function main(o, data) {
         .text(function(d) { return d.key; });
     t.append("tspan")
         .attr("dy", "1.0em")
-        .text(function(d) { return formatNumber(d.value); });
-    t.call(text);
+        .text(function(d) { return format_number(d.value); });
+    t.call(treemap_text);
 
     g.selectAll("rect")
-        .style("fill", function(d) { return color(d.key); });
+        .style("fill", function(d) { return treemap_color(d.key); });
 
-    function transition(d) {
-      if (transitioning || !d) return;
-      transitioning = true;
+    function transition(d)
+    {
+        if (transitioning || !d) return;
+        transitioning = true;
 
-      var g2 = display(d),
-          t1 = g1.transition().duration(750),
-          t2 = g2.transition().duration(750);
+        var g2 = treemap_display(d),
+            t1 = g1.transition().duration(750),
+            t2 = g2.transition().duration(750);
 
-      // Update the domain only after entering new elements.
-      x.domain([d.x, d.x + d.dx]);
-      y.domain([d.y, d.y + d.dy]);
+        // Update the domain only after entering new elements.
+        x.domain([d.x, d.x + d.dx]);
+        y.domain([d.y, d.y + d.dy]);
 
-      // Enable anti-aliasing during the transition.
-      svg.style("shape-rendering", null);
+        // Enable anti-aliasing during the transition.
+        svg.style("shape-rendering", null);
 
-      // Draw child nodes on top of parent nodes.
-      svg.selectAll(".depth").sort(function(a, b) { return a.depth - b.depth; });
+        // Draw child nodes on top of parent nodes.
+        svg.selectAll(".depth").sort(function(a, b) { return a.depth - b.depth; });
 
-      // Fade-in entering text.
-      g2.selectAll("text").style("fill-opacity", 0);
+        // Fade-in entering text.
+        g2.selectAll("text").style("fill-opacity", 0);
 
-      // Transition to the new view.
-      t1.selectAll(".ptext").call(text).style("fill-opacity", 0);
-      t1.selectAll(".ctext").call(text2).style("fill-opacity", 0);
-      t2.selectAll(".ptext").call(text).style("fill-opacity", 1);
-      t2.selectAll(".ctext").call(text2).style("fill-opacity", 1);
-      t1.selectAll("rect").call(rect);
-      t2.selectAll("rect").call(rect);
+        // Transition to the new view.
+        t1.selectAll(".ptext").call(treemap_text).style("fill-opacity", 0);
+        t1.selectAll(".ctext").call(treemap_text2).style("fill-opacity", 0);
+        t2.selectAll(".ptext").call(treemap_text).style("fill-opacity", 1);
+        t2.selectAll(".ctext").call(treemap_text2).style("fill-opacity", 1);
+        t1.selectAll("rect").call(treemap_rect);
+        t2.selectAll("rect").call(treemap_rect);
 
-      // Remove the old node when the transition is finished.
-      t1.remove().each("end", function() {
-        svg.style("shape-rendering", "crispEdges");
-        transitioning = false;
-      });
+        // Remove the old node when the transition is finished.
+        t1.remove().each("end", function() {
+            svg.style("shape-rendering", "crispEdges");
+            transitioning = false;
+        });
     }
 
     return g;
-  }
-
-  function text(text) {
-    text.selectAll("tspan")
-        .attr("x", function(d) { return x(d.x) + 6; })
-    text.attr("x", function(d) { return x(d.x) + 6; })
-        .attr("y", function(d) { return y(d.y) + 6; })
-        .style("opacity", function(d) { return this.getComputedTextLength() < x(d.x + d.dx) - x(d.x) ? 1 : 0; });
-  }
-
-  function text2(text) {
-    text.attr("x", function(d) { return x(d.x + d.dx) - this.getComputedTextLength() - 6; })
-        .attr("y", function(d) { return y(d.y + d.dy) - 6; })
-        .style("opacity", function(d) { return this.getComputedTextLength() < x(d.x + d.dx) - x(d.x) ? 1 : 0; });
-  }
-
-  function rect(rect) {
-    rect.attr("x", function(d) { return x(d.x); })
-        .attr("y", function(d) { return y(d.y); })
-        .attr("width", function(d) { return x(d.x + d.dx) - x(d.x); })
-        .attr("height", function(d) { return y(d.y + d.dy) - y(d.y); });
-  }
-
-  function name(d) {
-    return d.parent
-        ? name(d.parent) + " / " + d.key + " (" + formatNumber(d.value) + ")"
-        : d.key + " (" + formatNumber(d.value) + ")";
-  }
 }
+// ---------------
 
-/*if (window.location.hash === "") {
-    d3.json("br-data/test2.json", function(data) {
-        main({title: "Scholarships distribution"}, data);
-    });
-}
-*/
 
-// initialization
+// INITIALIZATION
 map.legend();
 update_map(slider.value);
 slider_num.innerHTML = slider.value;
 state_name.innerHTML = "Select a state for a detailed view";
+// ---------------
